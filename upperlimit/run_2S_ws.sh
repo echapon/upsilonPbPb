8efeb095dbcf893d87bc3eb3af862c38d72aa9d8
Emilien Chapon <emilien.chapon@cern.ch>
2015-02-12 12:18:28 +0100

    save the plots when make the workspaces

--- upperlimit/run_2S_ws.sh
+++ upperlimit/run_2S_ws.sh
@@ -2,4 +2,7 @@
 
 for cat in dimuPt0005000_dimuY000120 dimuPt0005000_dimuY120240 dimuPt000500_dimuY000240 dimuPt12002000_dimuY000240 dimuPt5001200_dimuY000240; do 
    root -l -b -q Raa2S_Workspace.C\(\"WS_pbpb_${cat}_fsr1_sigma0_ref2_fitres.root\"\,\"WS_pp_${cat}_fsr1_sigma0_ref2_fitres.root\"\,\"WS_combo2S_${cat}_fsr1_sigma0_ref2_fitres.root\"\)
+   mv c1.pdf c1_${cat}.pdf
+   mv c2.pdf c2_${cat}.pdf
+   mv cpoi.pdf cpoi_${cat}.pdf
 done
