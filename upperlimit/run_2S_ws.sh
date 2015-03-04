db64257c698d517ca7e12a3994e50655b87f20d8
Emilien Chapon <emilien.chapon@cern.ch>
2015-03-04 16:30:00 +0100

    Now we use 25 CPUs for WS creation: use ROOT with nice

--- upperlimit/run_2S_ws.sh
+++ upperlimit/run_2S_ws.sh
@@ -8,7 +8,7 @@ dir2=$dir/ptFits
 for cat in dimuPt000500 dimuPt12002000 dimuPt5001200; do 
    ppfile=`ls $dir2/WS*pp*$cat*root`
    pbpbfile=`ls $dir2/WS*pbpb*$cat*root`
-   root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
+   nice root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
    mv c1.pdf $dir2/c1_${cat}.pdf
    mv c2.pdf $dir2/c2_${cat}.pdf
    mv cpoi.pdf $dir2/cpoi_${cat}.pdf
@@ -19,7 +19,7 @@ dir2=$dir/rapidityFits
 for cat in dimuY000120 dimuY120240; do 
    ppfile=`ls $dir2/WS*pp*$cat*root`
    pbpbfile=`ls $dir2/WS*pbpb*$cat*root`
-   root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
+   nice root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
    mv c1.pdf $dir2/c1_${cat}.pdf
    mv c2.pdf $dir2/c2_${cat}.pdf
    mv cpoi.pdf $dir2/cpoi_${cat}.pdf
@@ -30,7 +30,7 @@ dir2=$dir/centralityFits
 for cat in cent0M5 cent5M10 cent10M20 cent20M30 cent30M40 cent40M50 cent50M100; do 
    ppfile=`ls $dir2/WS*pp*root`
    pbpbfile=`ls $dir2/WS*pbpb*$cat*root`
-   root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
+   nice root -l -b -q Raa2S_Workspace.C\(\"${pbpbfile}\"\,\"${ppfile}\"\,\"$dir2/WS_combo2S_${cat}.root\"\)
    mv c1.pdf $dir2/c1_${cat}.pdf
    mv c2.pdf $dir2/c2_${cat}.pdf
    mv cpoi.pdf $dir2/cpoi_${cat}.pdf
